a4df71c5015872faf824778f9de946e67df5c810
Hayden Rear <hayden.rear@gmail.com>
2021-11-22 17:09:14 -0500

    fix

--- src/test/scala/rules/RuleFactoryTest.scala
+++ src/test/scala/rules/RuleFactoryTest.scala
@@ -20,6 +20,11 @@ class RuleFactoryTest {
 
   @Test
   def testCreateRuleSequence(): Unit = {
-     assertThat(rules.length).isEqualTo(256)
+     assertThat(rules.length).isEqualTo(255)
+    rules.foreach(r => {
+      val sequence = r.sequence(r.initialState())
+      println(r.initialState().mkString("Array(", ", ", ")"))
+      Range(0,10).map(_ => println(sequence.next().mkString("Array(", ", ", ")")))
+    })
   }
 }
